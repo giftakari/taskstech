adc0bf0dabfa514788189a66bf1ba265d74e12c0
Godsgift Akari <giftakari@yahoo.com>
2021-07-22 18:24:51 +1000

    updte jobReducer.js

--- src/reducers/jobsReducer.js
+++ src/reducers/jobsReducer.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 import * as actions from "../actions/actionTypes";
-import { InitialState } from "../store/initialState";
 
-function jobsReducer(state = InitialState, action) {
+function jobsReducer(state = {}, action) {
   switch (action.type) {
     case actions.ADD_JOB:
       return [...state, action.payload];
@@ -11,22 +9,6 @@ function jobsReducer(state = InitialState, action) {
     default:
       return state;
   }
-=======
-import * as actions from '../actions/actionTypes';
-
-function jobsReducer(state = {}, action) {
-    switch (action.type) {
-        case actions.ADD_JOB:
-            return [
-                ...state,
-                action.payload
-            ];
-        case actions.GET_JOB_STATUS:
-            return action.payload
-        default:
-            return state;
-    }
->>>>>>> 51ec4fc566a850ed58c34e7f3ec9b0ee06e24aa8
 }
 
 export default jobsReducer;
